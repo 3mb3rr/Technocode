405883ee9aebaed87d6c3b600a78d23017cd39dd
3mb3rr <jsk130908@gmail.com>
2023-07-02 18:18:15 +0530

    Home jayveer2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionBase/poleDistanceDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionBase/poleDistanceDetection.java
@@ -1,100 +1,121 @@
-//package org.firstinspires.ftc.teamcode.VisionBase;
-//
-//import org.opencv.core.Core;
-//import org.opencv.core.Mat;
-//import org.opencv.core.MatOfPoint;
-//import org.opencv.core.Scalar;
-//import org.opencv.imgproc.Imgproc;
-//import org.openftc.easyopencv.OpenCvPipeline;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-////for dashboard
-///*@Config*/
-//public class poleDistanceDetection extends OpenCvPipeline {
-//
-//    //backlog of frames to average out to reduce noise
-//    ArrayList<double[]> frameList;
-//    //these are public static to be tuned in dashboard
-//    public static double strictLowS = 140;
-//    public static double strictHighS = 255;
-//
-//    public poleDistanceDetection() {
-//        frameList = new ArrayList<>();
-//    }
-//
-//    @Override
-//    public Mat processFrame(Mat input) {
-//        Mat mat = new Mat();
-//
-//        //mat turns into HSV value
-//        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
-//        if (mat.empty()) {
-//            return input;
-//        }
-//
-//        // lenient bounds will filter out near yellow, this should filter out all near yellow things(tune this if needed)
-//        Scalar lowHSV = new Scalar(20, 70, 80); // lenient lower bound HSV for yellow
-//        Scalar highHSV = new Scalar(32, 255, 255); // lenient higher bound HSV for yellow
-//
-//        Mat thresh = new Mat();
-//
-//        // Get a black and white image of yellow objects
-//        Core.inRange(mat, lowHSV, highHSV, thresh);
-//
-//        Mat masked = new Mat();
-//        //color the white portion of thresh in with HSV from mat
-//        //output into masked
-//        Core.bitwise_and(mat, mat, masked, thresh);
-//        //calculate average HSV values of the white thresh values
-//        Scalar average = Core.mean(masked, thresh);
-//
-//        Mat scaledMask = new Mat();
-//        //scale the average saturation to 150
-//        masked.convertTo(scaledMask, -1, 150 / average.val[1], 0);
-//
-//
-//        Mat scaledThresh = new Mat();
-//        //you probably want to tune this
-//        Scalar strictLowHSV = new Scalar(0, strictLowS, 0); //strict lower bound HSV for yellow
-//        Scalar strictHighHSV = new Scalar(255, strictHighS, 255); //strict higher bound HSV for yellow
-//        //apply strict HSV filter onto scaledMask to get rid of any yellow other than pole
-//        Core.inRange(scaledMask, strictLowHSV, strictHighHSV, scaledThresh);
-//
-//        Mat edges = new Mat();
+package org.firstinspires.ftc.teamcode.VisionBase;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//for dashboard
+/*@Config*/
+public class poleDistanceDetection extends OpenCvPipeline {
+
+    //backlog of frames to average out to reduce noise
+    ArrayList<double[]> frameList;
+    //these are public static to be tuned in dashboard
+    public static double strictLowS = 100;
+    public static double strictHighS = 255;
+    public double lArea = 0;
+    public int cx, cy;
+    public boolean inputtt, inputtt2 = false;
+
+    public int contourNo;
+    public poleDistanceDetection() {
+        frameList = new ArrayList<>();
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+        Mat mat = new Mat();
+        Mat output = input;
+
+        //mat turns into HSV value
+        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+        if (mat.empty()) {
+            return input;
+        }
+
+        // lenient bounds will filter out near yellow, this should filter out all near yellow things(tune this if needed)
+        Scalar lowHSV = new Scalar(15, 20, 5); // lenient lower bound HSV for yellow
+        Scalar highHSV = new Scalar(28, 255, 255); // lenient higher bound HSV for yellow
+
+        Mat thresh = new Mat();
+
+        // Get a black and white image of yellow objects
+        Core.inRange(mat, lowHSV, highHSV, thresh);
+
+        Mat masked = new Mat();
+        //color the white portion of thresh in with HSV from mat
+        //output into masked
+        Core.bitwise_and(mat, mat, masked, thresh);
+        //calculate average HSV values of the white thresh values
+        Scalar average = Core.mean(masked, thresh);
+
+        Mat scaledMask = new Mat();
+        //scale the average saturation to 150
+        masked.convertTo(scaledMask, -1, 150 / average.val[1], 0);
+
+
+        Mat scaledThresh = new Mat();
+        //you probably want to tune this
+        Scalar strictLowHSV = new Scalar(0, strictLowS, 0); //strict lower bound HSV for yellow
+        Scalar strictHighHSV = new Scalar(255, strictHighS, 255); //strict higher bound HSV for yellow
+        //apply strict HSV filter onto scaledMask to get rid of any yellow other than pole
+        Core.inRange(scaledMask, strictLowHSV, strictHighHSV, scaledThresh);
+
+        Mat edges = new Mat();
 //        //detect edges(only useful for showing result)(you can delete)
 //        Imgproc.Canny(scaledThresh, edges, 100, 200);
-//
-//        //contours, apply post processing to information
-//        List<MatOfPoint> contours = new ArrayList<>();
-//        Mat hierarchy = new Mat();
-//        //find contours, input scaledThresh because it has hard edges
-//        Imgproc.findContours(scaledThresh, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-//
-//        //list of frames to reduce inconsistency, not too many so that it is still real-time, change the number from 5 if you want
-//        if (frameList.size() > 5) {
-//            frameList.remove(0);
-//        }
-//
-//
-//        //release all the data
+
+        //contours, apply post processing to information
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        //find contours, input scaledThresh because it has hard edges
+        Imgproc.findContours(scaledThresh, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        for (int i = 0; i<contours.size(); i++){
+            if (Imgproc.contourArea(contours.get(contourNo))>lArea){ lArea = Imgproc.contourArea(contours.get(i));
+                contourNo = i;
+            }
+        }
+
+        Moments M = new Moments();
+        M =Imgproc.moments(contours.get(contourNo));
+        cx = (int)(M.m10/M.m00);
+        cy = (int)(M.m01/M.m00);
+        Imgproc.drawContours(edges, contours, 1, new Scalar(0, 255, 0), 3);
+        //list of frames to reduce inconsistency, not too many so that it is still real-time, change the number from 5 if you want
+        if (frameList.size() > 5) {
+            frameList.remove(0);
+        }
+        Imgproc.line(input, new Point(cx, cy), new Point(cx, 0), new Scalar(0, 255, 255));
+        Imgproc.line(scaledThresh, new Point(cx, cy), new Point(cx, 0), new Scalar(0, 255, 255));
+        //release all the data
 //        input.release();
-//        scaledThresh.copyTo(input);
-//        scaledThresh.release();
-//        scaledMask.release();
-//        mat.release();
-//        masked.release();
-//        edges.release();
-//        thresh.release();
-//        finalMask.release();
-//        //change the return to whatever mat you want
-//        //for example, if I want to look at the lenient thresh:
-//        // return thresh;
-//        // note that you must not do thresh.release() if you want to return thresh
-//        // you also need to release the input if you return thresh(release as much as possible)
-//        return input;
-//    }
-//
-//
-//}
+        if(inputtt){
+            scaledThresh.copyTo(input);}
+        if(inputtt2){
+            masked.copyTo(input);}
+        scaledThresh.release();
+        scaledMask.release();
+        mat.release();
+        masked.release();
+        edges.release();
+        thresh.release();
+
+        //change the return to whatever mat you want
+        //for example, if I want to look at the lenient thresh:
+        // return thresh;
+        // note that you must not do thresh.release() if you want to return thresh
+        // you also need to release the input if you return thresh(release as much as possible)
+        return input;
+    }
+
+
+
+}
